4a93310ad59adde22a809748cb068ec559222a32
judiciousmurich <murichjudicious45@gmail.com>
2023-06-21 12:36:40 +0300

    added pages

--- Client/src/pages/contact/Contact.jsx
+++ Client/src/pages/contact/Contact.jsx
@@ -1,13 +1,29 @@
-import './Contact.css';
 
-function Contact() {
+import "./Contact.css";
+
+const Contact = () => {
   return (
-    <div>
-      <h2>Contact Us</h2>
-      <p>Get in touch with us:</p>
-  
-    </div>
+    <>
+      <section className="contact mb">
+        <div className="contact-background">
+        <h1 className="title">Contact Us</h1>
+            <p className="subtitle">Get Helps & Friendly Support</p>
+        </div>
+        <div className="container">
+          <form className="shadow">
+            <h4>Fill Up The Form</h4>
+            <div>
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Email" />
+            </div>
+            <input type="text" placeholder="Subject" />
+            <textarea cols="30" rows="10"></textarea>
+            <button>Submit Request</button>
+          </form>
+        </div>
+      </section>
+    </>
   );
-}
+};
 
 export default Contact;
--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 
-import './Home.css'; 
-function Home() {
+import backgroundImage from '../images/home-1.jpg';
+import './Home.css';
+
+const Home = () => {
   return (
-    <div>
-      <h1>Welcome to Dream House App</h1>
-      <p>Find your dream house today!</p>
-    </div>
+    <section className="home" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="container">
+        <h1 className="title">MODERN HOUSE</h1>
+        <p className="subtitle">MAKE BETTER LIFE</p>
+        <p className="description">Discover your perfect home with the Dream House app. Browse, customize, and visualize your dream house effortlessly..</p>
+        <button className="btn">Learn More </button>
+      </div>
+    </section>
   );
 }
 
