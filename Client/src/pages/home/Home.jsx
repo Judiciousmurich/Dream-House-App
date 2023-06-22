f79f36910e35b984d126bb52cb8c885952436bcb
judiciousmurich <murichjudicious45@gmail.com>
2023-06-23 00:01:50 +0300

    updated changes

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -1,8 +1,17 @@
-
-
+import { Link } from 'react-router-dom';
+import './Home.css'
 function Home() {
   return (
-    <div>Home</div>
+  <main className="home-container" id="home">
+    <div className="home-text">
+      <h1>Modern House <br/>Make Better life</h1>
+      <p>Search for apartments, houses and rooms in your area.<br/>Browse, customize, and visualize your dream house effortlessly</p>
+      
+      <Link to="/search"><button type="submit" className='btn-home'>Get Started!</button></Link>
+  
+
+    </div>
+  </main>
   )
 }
 
